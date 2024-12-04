556055989525f843055d238c31290c95a0142c86
haozhen <whaozhen01@gmail.com>
2024-12-04 13:56:58 +0800

    Add Country and Filter
    Added components for App.jsx to render.

---
+++ part2/countries/src/components/Filter.jsx
@@ -0,0 +1,10 @@
+const Filter = ({ filter, handleFilterChange }) => {
+    return (
+      <div>
+        find countries
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
+    );
+  };
+  
+export default Filter;
