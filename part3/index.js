579177874c78e6de16714d7e33276c54affd56d3
haozhen <whaozhen01@gmail.com>
2024-12-05 19:45:34 +0800

    Add single note fetch

--- part3/index.js
+++ part3/index.js
@@ -26,6 +26,17 @@ app.get('/', (request, response) => {
   app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
   
   const PORT = 3001
   app.listen(PORT, () => {
