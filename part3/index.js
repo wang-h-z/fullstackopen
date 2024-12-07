20e888c5b1d0d82455f75550d47bd493733388a2
haozhen <whaozhen01@gmail.com>
2024-12-07 10:54:52 +0800

    Complete Exercise 3.3

--- part3/index.js
+++ part3/index.js
@@ -55,7 +55,7 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const person = persons.find(person => person.id === id)
+  const person = persons.find(p => p.id === id)
   
   if (person) {
     response.json(person)
