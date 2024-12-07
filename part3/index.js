a521e7bb2a31b656516ae4c8f5aec239ee8d8010
haozhen <whaozhen01@gmail.com>
2024-12-07 10:43:46 +0800

    Complete Exercise 3.1

--- part3/index.js
+++ part3/index.js
@@ -1,30 +1,38 @@
 const express = require('express')
 const app = express()
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Phonebook!!</h1>')
   })
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
   })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -36,6 +44,33 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: Boolean(body.important) || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
