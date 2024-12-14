6eb3d3b9adcf4b8fefb6635ea970c0b9f582eab3
haozhen <whaozhen01@gmail.com>
2024-12-14 17:52:09 +0800

    Complete Exercise 3.21 and 3.22

--- part3/eslint.config.mjs
+++ part3/eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -13,5 +14,38 @@ export default [
       },
       ecmaVersion: "latest",
     },
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+    'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+    },
+  },
+  { 
+    ignores: ["dist/**", "build/**"],
   },
 ]
