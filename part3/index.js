3b8b6df31dbf186d9cb71eb7e4f6725b01056636
haozhen <whaozhen01@gmail.com>
2024-12-14 14:41:52 +0800

    Abstract mongoose logic to person.js in ./models

--- part3/index.js
+++ part3/index.js
@@ -1,13 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
-mongoose.set('strictQuery',false)
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url)
 const app = express()
 
 app.use(express.json())
@@ -44,20 +38,7 @@ let persons = [
   }
 ]
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
+const Person = require('./models/person')
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook!!</h1>')
