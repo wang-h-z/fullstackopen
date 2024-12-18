53709126a64f8008cdd25fee86d613842721d8e7
haozhen <whaozhen01@gmail.com>
2024-12-18 16:00:36 +0800

    Update get request to async/await

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,17 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
   
-  blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
-  
+    
     blog
       .save()
       .then(result => {
