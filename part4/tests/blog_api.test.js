e00cc31416919325e2b8aa376d2b3dc82f362599
haozhen <whaozhen01@gmail.com>
2024-12-18 15:55:12 +0800

    Add logic to initialise database before test

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,27 @@
+const { test, describe, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+describe('testing GET requests', () => {
+    test('blogs are returned as json', async () => {
+        await api
+          .get('/api/blogs')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+        })
+      
+    after(async () => {
+            await mongoose.connection.close()
+    })
+})
+
+
