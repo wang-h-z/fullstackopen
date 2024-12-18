f675bf9e8e4574729b1e5a5a6425b8bd54ea5106
haozhen <whaozhen01@gmail.com>
2024-12-18 14:14:01 +0800

    Complete Exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,6 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const blog = blogs.reduce((favourite, current) =>
+    favourite.likes > current.likes ? favourite : current
+  )
+  return { title: blog.title, author: blog.author, likes: blog.likes };
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
