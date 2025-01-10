d677bedf5c919763e00badaee85e4f872575072d
haozhen <whaozhen01@gmail.com>
2025-01-10 11:33:11 +0800

    Complete Exercise 5.14

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -20,22 +20,24 @@ const Blog = ({ blog, addLike, deleteBlog, user }) => {
   const canDelete = blog.user?.username === user?.username
 
   return (
-  <div className='blog'>
-    <div style={{...blogStyle, ...hideWhenVisible}}>
-      {blog.title} {blog.author}
-      <button onClick={() => setShowDetails(!showDetails)}>view</button>
-    </div>
-    <div style={{ ...blogStyle, ...showWhenVisible }}>
-      {blog.title} {blog.author}
-      <button onClick={() => setShowDetails(!showDetails)}>hide</button>
-      <div>{blog.url}</div>
-      <div>likes {blog.likes} <button onClick={handleLike}>like</button></div>
-      <div>{blog.user?.username}</div>
-      {canDelete && (
-            <button onClick={() => deleteBlog(blog)}>remove</button>
+    <div className="blog">
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button onClick={handleLike}>like</button>
+          </div>
+          <div>{blog.user?.username}</div>
+          {canDelete && <button onClick={() => deleteBlog(blog)}>remove</button>}
+        </div>
       )}
-    </div> 
-  </div>  
-)}
+    </div>
+    </div>
+  )}
 
 export default Blog
--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog';
 
 const blog = {
@@ -19,8 +20,28 @@ const user = {
 
 test('renders title and author, but not url and likes by default', () => {
     render(<Blog blog={blog} user={user} />);
-    
-    expect(screen.getAllByText(`${blog.title} ${blog.author}`)[0]).toBeInTheDocument();
-    expect(screen.getByText(blog.url)).not.toBeVisible();
-    expect(screen.getByText(`likes ${blog.likes}`)).not.toBeVisible();
+
+    expect(screen.getByText(`${blog.title} ${blog.author}`)).toBeVisible();
+ 
+    expect(screen.queryByText(blog.url)).toBeNull();
+    expect(screen.queryByText(`likes ${blog.likes}`)).toBeNull();
   });
+
+test('url and likes are shown when view button is pressed', async () => {
+    const mockHandler = vi.fn();
+    render(<Blog blog={blog} user={user} addLike={mockHandler} deleteBlog={mockHandler} />)
+  
+    const userEventHandler = userEvent.setup()
+  
+    const viewButton = screen.getByText('view')
+    expect(viewButton).toBeInTheDocument()
+  
+    expect(screen.queryByText(blog.url)).not.toBeInTheDocument()
+    expect(screen.queryByText(`likes ${blog.likes}`)).not.toBeInTheDocument()
+  
+    await userEventHandler.click(viewButton)
+  
+    expect(screen.getByText(blog.url)).toBeInTheDocument()
+    expect(screen.getByText(`likes ${blog.likes}`)).toBeInTheDocument()
+})
+  
