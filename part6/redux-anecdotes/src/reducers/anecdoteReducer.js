3d2903a9bed75f024800f730fec7b0454cee767a
haozhen <whaozhen01@gmail.com>
2025-01-14 10:13:25 +0800

    Update Redux reducer
    Updated redux reducer with VOTE action.
    The VOTE action increases the vote of an anecdote by 1.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,22 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch (action.type) {
+    case 'VOTE': {
+      return state.map(anecdote => anecdote.id === action.payload.id
+                        ? {...anecdote, votes: votes + 1}
+                        : anecdote
+                      )
+    }
+    default: return state
+  }
+}
 
-  return state
+export const upVote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: {id}
+  }
 }
 
 export default reducer
