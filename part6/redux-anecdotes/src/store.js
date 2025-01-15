ed2c4eebe5d5cfb59817e4776d7d99fcf7c24012
haozhen <whaozhen01@gmail.com>
2025-01-16 01:05:09 +0800

    Complete Exercise 6.12

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,12 +1,14 @@
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import anecdoteReducer from './reducers/anecdoteReducer';
+import filterReducer from './reducers/filterReducer';
+import notificationReducer from './reducers/notificationReducer';
 
 const store = configureStore({
-    reducer: {
-        anecdotes: anecdoteReducer,
-        filter: filterReducer
-    }
-})
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+    notification: notificationReducer
+  }
+});
 
-export default store
+export default store;
